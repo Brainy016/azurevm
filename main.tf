5b9f5362d3ec7deafdd437438f24407f953fedd5
Brainy016 <danadeniji13@gmail.com>
2024-10-20 23:14:46 +0100

    commit

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ resource "azurerm_resource_group" "brainy_resource" {
   location = "West Europe"
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "acceptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.brainy_resource.name
-  location            = azurerm_resource_group.brainy_resource.location
-  allocation_method   = "Static"
+# resource "azurerm_public_ip" "example" {
+#   name                = "acceptanceTestPublicIp1"
+#   resource_group_name = azurerm_resource_group.brainy_resource.name
+#   location            = azurerm_resource_group.brainy_resource.location
+#   allocation_method   = "Static"
 
-  
-}
+
+# }
 resource "azurerm_virtual_network" "brainy_resource" {
   name                = "brainy_resource-network"
   address_space       = ["10.0.0.0/16"]
@@ -39,6 +39,16 @@ resource "azurerm_network_interface" "brainy_resource" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.brainy_resource.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.nic_ip.id
+  }
+}
+resource "azurerm_public_ip" "nic_ip" {
+  name                = "public_ip"
+  resource_group_name = azurerm_resource_group.brainy_resource.name
+  location            = azurerm_resource_group.brainy_resource.location
+  allocation_method   = "Dynamic"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -51,6 +61,55 @@ resource "azurerm_linux_virtual_machine" "brainy_resource" {
   network_interface_ids = [
     azurerm_network_interface.brainy_resource.id,
   ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = data.azurerm_ssh_public_key.vm_key.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+#creating the second vm
+resource "azurerm_public_ip" "sapphire_public_ip" {
+  name                = "public_ip"
+  resource_group_name = azurerm_resource_group.brainy_resource.name
+  location            = azurerm_resource_group.brainy_resource.location
+  allocation_method   = "Dynamic"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "azurerm_network_interface" "sapphire_nic" {
+  name                = "sapphire-nic"
+  location            = azurerm_resource_group.brainy_resource.location
+  resource_group_name = azurerm_resource_group.brainy_resource.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.brainy_resource.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.sapphire_public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "sapphire" {
+  name                = "Secondvm"
+  resource_group_name = azurerm_resource_group.brainy_resource.name
+  location            = azurerm_resource_group.brainy_resource.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [azurerm_network_interface.sapphire_nic.id]
 
   admin_ssh_key {
     username   = "adminuser"
