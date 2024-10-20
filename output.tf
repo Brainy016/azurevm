982d73fb596f729f6cb8b952fa255058006774b9
Brainy016 <danadeniji13@gmail.com>
2024-10-20 23:16:37 +0100

    output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "azure_ip_addr" {
   value = azurerm_linux_virtual_machine.brainy_resource.public_ip_address
 }
+output "azure_ip_addrress" {
+  value = azurerm_linux_virtual_machine.sapphire.public_ip_address
+}
+
